9166a5b56f432bc85058c2b883c8f7304568bae1
marcus <marcustanyonghan@gmail.com>
2026-03-20 16:14:00 +0800

    uibuild

--- part2/phonebook/src/main.jsx
+++ part2/phonebook/src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDom from "react-dom/client";
-// import App from "./app";
+import App from "./App.jsx";
 import "./index.css";
 
 ReactDom.createRoot(document.getElementById("root")).render(<App />);
